ad4db3d7930251b0519401059b56acf20fd5e37f
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-02-01 13:44:58 -0300

    chore: About page setup

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
+            nobis dignissimos quasi et quod, repudiandae aliquam porro nostrum
+            animi fugit quaerat illum voluptates, reprehenderit eos unde
+            doloribus sed harum alias? Reprehenderit laudantium iure amet, illum
+            temporibus placeat ipsam unde sed sapiente esse quam aspernatur
+            quasi dolorem excepturi perspiciatis quas accusamus impedit qui
+            velit necessitatibus. Doloremque laborum, voluptatem odit libero
+            veniam dolor voluptatum ratione impedit eaque! Odio eos aut quae
+            fugiat.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +57,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
