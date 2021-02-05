485b68fdd300013f3189e2ed8212e8851b536479
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-02-05 13:38:43 -0300

    chore: Authwrapper to improve checkout access

--- src/App.js
+++ src/App.js
@@ -11,36 +11,39 @@ import {
   Error,
   Checkout,
   PrivateRoute,
+  AuthWrapper,
 } from './pages';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route path='/' exact>
-          <Home />
-        </Route>
-        <Route path='/about' exact>
-          <About />
-        </Route>
-        <Route path='/products' exact>
-          <Products />
-        </Route>
-        <Route path='/cart' exact>
-          <Cart />
-        </Route>
-        <PrivateRoute path='/checkout' exact>
-          <Checkout />
-        </PrivateRoute>
-        <Route path='/products/:id' exact children={<SingleProduct />} />
-        <Route path='*'>
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+          <Route path='/about' exact>
+            <About />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/cart' exact>
+            <Cart />
+          </Route>
+          <PrivateRoute path='/checkout' exact>
+            <Checkout />
+          </PrivateRoute>
+          <Route path='/products/:id' exact children={<SingleProduct />} />
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
